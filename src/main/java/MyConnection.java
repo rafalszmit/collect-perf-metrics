48a39b97662251a8bec053a5377ed22a69c7860a
rafalszmit <rafal.szmit@testarmy.com>
2018-06-28 14:38:39 +0200

    Custom limits

--- src/main/java/MyConnection.java
+++ src/main/java/MyConnection.java
@@ -13,191 +13,225 @@ import java.util.stream.Stream;
 
 public class MyConnection {
 
-    static String CONFIG_FILE = "config.properties";
+	static String CONFIG_FILE = "config.properties";
 
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        MyConnection http = new MyConnection();
+		MyConnection http = new MyConnection();
 
-        String url = getUrl(CONFIG_FILE);
-        List<String> postParameters = getUrlParameters(CONFIG_FILE);
+		String url = getUrl(CONFIG_FILE);
+		List<String> postParameters = getUrlParameters(CONFIG_FILE);
+		List<Double> targetLimits = getLimits(CONFIG_FILE);
 
-        postParameters.forEach(postParameter -> {
+		for (int i = 0; i < postParameters.size(); i++) {
+			try {
+				String metricData = http.sendPost(url, postParameters.get(i));
+				Map<String, List<Double>> measurementsPerSite = parseMeasurementsPerSite(metricData);
+				Map<String, List<Double>> statsPerSite = calculateStatsPerSite(measurementsPerSite,
+						targetLimits.get(i));
+				String output = prepareOutput(postParameters.get(i), statsPerSite);
+				saveToFile(output, "Dane.txt");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-            try {
-                String metricData = http.sendPost(url, postParameter);
-                Map<String, List<Double>> measurementsPerSite = parseMeasurementsPerSite(metricData);
-                Map<String, List<Double>> statsPerSite = calculateStatsPerSite(measurementsPerSite);
-                String output = prepareOutput(postParameter, statsPerSite);
-                saveToFile(output, "Dane.txt");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-    }
+	private String sendPost(String url, String urlParameters) throws Exception {
 
-    private String sendPost(String url, String urlParameters) throws Exception {
+		URL u = new URL(url);
 
-        URL u = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) u.openConnection();
+		con.setDoOutput(true);
+		con.setRequestMethod("POST");
 
-        HttpURLConnection con = (HttpURLConnection) u.openConnection();
-        con.setDoOutput(true);
-        con.setRequestMethod("POST");
+		byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+		int postDataLength = postData.length;
 
-        byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
-        int postDataLength = postData.length;
+		con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+		con.setRequestProperty("charset", "utf-8");
+		con.setRequestProperty("Content-Length", Integer.toString(postDataLength));
 
-        con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-        con.setRequestProperty("charset", "utf-8");
-        con.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+		try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+			wr.write(postData);
+		}
 
-        try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
-            wr.write(postData);
-        }
+		BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while ((line = br.readLine()) != null) {
+			sb.append(line + "\n");
+		}
+		br.close();
+		String grafData = sb.toString();
+		return grafData;
+	}
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = br.readLine()) != null) {
-            sb.append(line + "\n");
-        }
-        br.close();
-        String grafData = sb.toString();
-        return grafData;
-    }
+	private static Map<String, List<Double>> parseMeasurementsPerSite(String grafData) {
 
-    private static Map<String, List<Double>> parseMeasurementsPerSite(String grafData) {
+		JSONArray response = new JSONArray(grafData);
+		Map<String, List<Double>> outputList = new HashMap<>();
+		List<Double> valuePerSite = new ArrayList<Double>();
 
-        JSONArray response = new JSONArray(grafData);
-        Map<String, List<Double>> outputList = new HashMap<>();
-        List<Double> valuePerSite = new ArrayList<Double>();
+		for (Object obj : response) {
+			String targetName = ((JSONObject) obj).getString("target");
+			JSONArray datapoints = ((JSONObject) obj).getJSONArray("datapoints");
+			Stream<Double> pointValuesStream = datapoints.toList().stream()
+					.filter(el -> ((ArrayList) el).get(0) != null)
+					.map(el -> (Double.parseDouble(((ArrayList) el).get(0).toString())));
+			valuePerSite = pointValuesStream.collect(Collectors.toList());
+			outputList.put(targetName, valuePerSite);
+		}
 
-        for (Object obj : response) {
-            String targetName = ((JSONObject) obj).getString("target");
-            JSONArray datapoints = ((JSONObject) obj).getJSONArray("datapoints");
-            Stream<Double> pointValuesStream = datapoints.toList().stream()
-                    .filter(el -> ((ArrayList) el).get(0) != null)
-                    .map(el -> (Double.parseDouble(((ArrayList) el).get(0).toString())));
-            valuePerSite = pointValuesStream.collect(Collectors.toList());
-            outputList.put(targetName, valuePerSite);
-        }
+		return outputList;
+	}
 
-        return outputList;
-    }
+	private static String prepareOutput(String header, Map<String, List<Double>> sitesWithStats) {
+		String LF = System.getProperty("line.separator");
+		StringBuffer output = new StringBuffer();
+		output.append(header + LF);
+		output.append("site,min,max,mean,stdev" + LF);
+		for (Map.Entry<String, List<Double>> entry : sitesWithStats.entrySet()) {
+			String site = entry.getKey();
+			List<Double> stats = entry.getValue();
+			Stream<Double> statsStream = stats.stream();
+			output.append(
+					site + "," + statsStream.map(x -> ((Double) x).toString()).collect(Collectors.joining(",")) + LF);
+		}
+		output.append(LF);
+		return output.toString();
+	}
 
-    private static String prepareOutput(String header, Map<String, List<Double>> sitesWithStats) {
-        String LF = System.getProperty("line.separator");
-        StringBuffer output = new StringBuffer();
-        output.append(header + LF);
-        output.append("site,min,max,mean,stdev" + LF);
-        for (Map.Entry<String, List<Double>> entry : sitesWithStats.entrySet()) {
-            String site = entry.getKey();
-            List<Double> stats = entry.getValue();
-            Stream<Double> statsStream = stats.stream();
-            output.append(site + "," + statsStream.map(x -> ((Double) x).toString()).collect(Collectors.joining(",")) + LF);
-        }
-        output.append(LF);
-        return output.toString();
-    }
+	private static void saveToFile(String output, String fileName) {
 
-    private static void saveToFile(String output, String fileName) {
+		try {
+			FileWriter dataFile;
+			PrintWriter dataWriter;
+			File userDataFile = new File(fileName);
+			if (userDataFile.exists()) {
+				dataFile = new FileWriter(fileName, true);
+				System.out.println("Added to existing file: " + fileName);
+			} else {
+				dataFile = new FileWriter(fileName);
+				System.out.println("Created new file: " + fileName);
+			}
+			dataWriter = new PrintWriter(dataFile);
+			dataWriter.println(output);
+			dataWriter.close();
 
-        try {
-            FileWriter dataFile;
-            PrintWriter dataWriter;
-            File userDataFile = new File(fileName);
-            if (userDataFile.exists()) {
-                dataFile = new FileWriter(fileName, true);
-                System.out.println("Added to existing file: " + fileName);
-            } else {
-                dataFile = new FileWriter(fileName);
-                System.out.println("Created new file: " + fileName);
-            }
-            dataWriter = new PrintWriter(dataFile);
-            dataWriter.println(output);
-            dataWriter.close();
+		} catch (Exception e) {
+			System.out.println("File Error");
+		}
+	}
 
-        } catch (Exception e) {
-            System.out.println("File Error");
-        }
-    }
+	private static Map<String, List<Double>> calculateStatsPerSite(Map<String, List<Double>> inputedMap, Double limit) {
 
-    private static Map<String, List<Double>> calculateStatsPerSite(Map<String, List<Double>> inputedMap) {
+		DescriptiveStatistics stats = new DescriptiveStatistics();
 
-        DescriptiveStatistics stats = new DescriptiveStatistics();
+		Map<String, List<Double>> result = inputedMap.entrySet().stream()
+				.collect(Collectors.toMap(x -> x.getKey(), x -> {
+					List<Double> value = x.getValue();
 
-        Map<String, List<Double>> result = inputedMap.entrySet().stream().collect(
-                Collectors.toMap(x -> x.getKey(), x -> {
-                            List<Double> value = x.getValue();
-                            List<Double> filteredStatsStream = value.stream().filter(val -> val < 10000).collect(Collectors.toList());
-                            filteredStatsStream.forEach(val -> stats.addValue(val));
-                            return Arrays.asList(stats.getMin(), stats.getMax(), stats.getMean(), stats.getStandardDeviation());
-                        }
-                ));
+					List<Double> filteredStatsStream = value.stream().filter(val -> val < limit)
+							.collect(Collectors.toList());
+					filteredStatsStream.forEach(val -> stats.addValue(val));
 
-        return result;
-    }
+					return Arrays.asList(stats.getMin(), stats.getMax(), stats.getMean(), stats.getStandardDeviation());
 
-    private static String getUrl(String fileName) {
+				}));
 
-        InputStream input = null;
-        String url = null;
-        try {
-            input = new FileInputStream(fileName);
-            Properties prop = new Properties();
-            prop.load(input);
-            String scheme = prop.getProperty("scheme");
-            String domain = prop.getProperty("domain");
-            String port = prop.getProperty("port");
-            String path = prop.getProperty("path");
-            url = scheme + "://" + domain + ":" + port + "/" + path;
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        } finally {
-            if (input != null) {
-                try {
-                    input.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+		return result;
+	}
 
-        return url;
-    }
+	private static String getUrl(String fileName) {
 
-    private static List<String> getUrlParameters(String fileName) {
+		InputStream input = null;
+		String url = null;
+		try {
+			input = new FileInputStream(fileName);
+			Properties prop = new Properties();
+			prop.load(input);
+			String scheme = prop.getProperty("scheme");
+			String domain = prop.getProperty("domain");
+			String port = prop.getProperty("port");
+			String path = prop.getProperty("path");
+			url = scheme + "://" + domain + ":" + port + "/" + path;
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 
-        InputStream input = null;
-        List<String> postParameters = new LinkedList<String>();
+		return url;
+	}
 
-        try {
-            input = new FileInputStream(fileName);
-            Properties prop = new Properties();
-            prop.load(input);
-            String from = prop.getProperty("from");
-            String until = prop.getProperty("until");
-            String format = prop.getProperty("format");
+	private static List<String> getUrlParameters(String fileName) {
 
-            int amoutOfTargets = Integer.parseInt(prop.getProperty("amoutOfTargets"));
-            for (int i = 1; i < amoutOfTargets + 1; i++) {
-                String target = prop.getProperty("target." + i);
-                target = URLEncoder.encode(target, "UTF-8");
-                postParameters.add("target=" + target + "&from=" + from + "&until=" + until + "&format=" + format);
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        } finally {
-            if (input != null) {
-                try {
-                    input.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+		InputStream input = null;
+		List<String> postParameters = new LinkedList<String>();
 
-        return postParameters;
-    }
+		try {
+			input = new FileInputStream(fileName);
+			Properties prop = new Properties();
+			prop.load(input);
+			String from = prop.getProperty("from");
+			String until = prop.getProperty("until");
+			String format = prop.getProperty("format");
+
+			int amoutOfTargets = Integer.parseInt(prop.getProperty("amoutOfTargets"));
+			for (int i = 1; i < amoutOfTargets + 1; i++) {
+				String target = prop.getProperty("target." + i);
+				target = URLEncoder.encode(target, "UTF-8");
+				postParameters.add("target=" + target + "&from=" + from + "&until=" + until + "&format=" + format);
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return postParameters;
+	}
+
+	private static List<Double> getLimits(String fileName) {
+		InputStream input = null;
+		List<Double> result = new ArrayList<>();
+
+		try {
+			input = new FileInputStream(fileName);
+			Properties prop = new Properties();
+			prop.load(input);
+			int amoutOfTargets = Integer.parseInt(prop.getProperty("amoutOfTargets"));
+			for (int i = 1; i < amoutOfTargets + 1; i++) {
+				// String target = prop.getProperty("target." + i);
+				Double limit = Double.parseDouble(prop.getProperty("targetLimit." + i));
+				// target = URLEncoder.encode(target, "UTF-8");
+				result.add(limit);
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return result;
+	}
 
 }
